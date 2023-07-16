3773734f602a5ff8be4d790831f731a89dc6ff8a
Joseph <iossif123gg@gmail.com>
2023-07-16 14:39:26 +0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <div className="text-3xl">Hello World</div>
+  </StrictMode>
+);
