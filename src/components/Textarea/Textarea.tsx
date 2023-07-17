cc2f6a441d155a101f61e2b361ea83a8a07da840
Iossif Benbassat <iossif.benbassat@strypes.eu>
2023-07-17 09:28:03 +0300

    add autofocus attribute to textarea

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -9,6 +9,7 @@ export default function Textarea({ value, onChange }: Props) {
   return (
     <>
       <textarea
+        autoFocus
         className="textarea textarea-lg w-full max-w-5xl border-zinc-400"
         rows={6}
         value={value}
