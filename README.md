febdbcbcf446ffa8a5fc3ec1128e00a2ce669175
Joseph <iossif123gg@gmail.com>
2023-07-17 23:23:17 +0300

    remove controls stupid hook

--- README.md
+++ README.md
@@ -18,8 +18,9 @@ Each version has its own branch with the same name - i.e `0.0.1`
 - **0.0.1** upper case, lower case, reset
 - **0.0.2** copy functionality and auto focus on textarea
 - **0.0.3** add snake case functionality
+- **0.0.4** add camelCase and PascalCase functionality
 
 ## Ideas
-- **0.0.4** add camelCase and PascalCase functionality
+- **0.0.5** refactor `useGetControls`. Probably should have some kind of a config. Dunno yet. 
 
 ## Planned
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import { useState } from "react";
 import { SetValue } from "./types";
 import Textarea from "./components/Textarea/Textarea";
-
 import Copy from "./components/Copy/Copy";
-import useGetControls from "./hooks/useGetControls";
+import * as transformers from "./transformers/transformers";
+
+enum BUTTON_LABELS {
+  UPPER_CASE = "UPPER CASE",
+  LOWER_CASE = "lower case",
+  SNAKE_CASE = "snake_case",
+  PASCAL_CASE = "PascalCase",
+  CAMEL_CASE = "camelCase",
+}
+
+const transformFuncs = {
+  [BUTTON_LABELS.UPPER_CASE]: transformers.toUpperCase,
+  [BUTTON_LABELS.LOWER_CASE]: transformers.toLowerCase,
+  [BUTTON_LABELS.SNAKE_CASE]: transformers.toSnakeCase,
+  [BUTTON_LABELS.PASCAL_CASE]: transformers.toPascalCase,
+  [BUTTON_LABELS.CAMEL_CASE]: transformers.toCamelCase,
+};
+
+const controls = Object.entries(transformFuncs).map(
+  ([label, transformFunc]) => ({
+    label,
+    onClick: (value: string, callback: SetValue) => {
+      callback(transformFunc(value));
+    },
+  })
+);
 
 const copyText = (value: string) => {
   navigator.clipboard.writeText(value);
@@ -12,7 +36,6 @@ const copyText = (value: string) => {
 export default function App() {
   const [value, dispatchSetValue] = useState("");
   const setValue: SetValue = (newValue) => dispatchSetValue(newValue);
-  const controls = useGetControls(setValue);
 
   return (
     <div className="p-8">
@@ -26,7 +49,7 @@ export default function App() {
           {controls.map(({ label, onClick }) => (
             <button
               key={label}
-              onClick={onClick}
+              onClick={() => onClick(value, setValue)}
               className="btn btn-neutral normal-case"
             >
               {label}
